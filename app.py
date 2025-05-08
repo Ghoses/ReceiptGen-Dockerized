26e57493a10090830d4e7f68ac8aeec6e43b70bc
Ghoses <52277594+Ghoses@users.noreply.github.com>
2025-05-09 01:54:59 +0200

    Performance improvements
    debug false

--- app.py
+++ app.py
@@ -443,4 +443,4 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
